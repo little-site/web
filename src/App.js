893437ab3c12489480bf91acf813a38e120b0d15
benjaminsehl <ben@little.site>
2019-10-17 12:50:37 +0200

    Add Home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import Posts from './controllers/Posts';
 import Post from './controllers/Post';
 import SignIn from './controllers/SignIn';
+import Index from './views/Home';
 
 class App extends Component {
   render() {
@@ -20,6 +21,10 @@ class App extends Component {
             path="/:site/:slug"
             component={props => <Post {...props} />}
           />
+          <Route
+            exact
+            path="/"
+            component={Index}
         </Switch>
       </Router>
     );
